b078da258876549a749b6ddf7931bb946bb319ea
orangeparrotcarrot <72738134+orangeparrotcarrot@users.noreply.github.com>
2022-01-03 10:34:11 +0000

    server working, added server log

--- CW/Networks/client.py
+++ CW/Networks/client.py
@@ -3,6 +3,7 @@ import sys
 import threading
 
 def getParameters():
+    #gets the ip address, user nickname and port number from the command line.
     try:
         ip = sys.argv[1]
         user = sys.argv[2]
@@ -15,32 +16,46 @@ def getParameters():
     except:
         print("Port number (third input) must be an integer")
         sys.exit()
+    if user == '':
+        print('Please enter a username')
+        sys.exit()
     return ip, user, port
 
 def receiveMessage():
-    while True:                                                 #making valid connection
+    #receive a message
+    while 1:
         try:
             message = clientSocket.recv(1024).decode()
-            if message == 'NICKNAME':
+            if message == 'USERNAME':
                 clientSocket.sendall(user.encode())
             else:
                 print(message)
-        except:                                                 #case on wrong ip/port details
+        except:
             print("An error occured!")
             clientSocket.close()
             break
 
 def writeMessage():
-    while True:                                          #message layout
-        message = '{}: {}'.format(user, input(''))
+    #send a message
+    while 1:
+        message = input('')
+        message = f'{user}: {message}'
         clientSocket.sendall(message.encode())
 
-ip, user, port = getParameters()
 
-clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      #socket initialization
-clientSocket.connect((ip, port))                             #connecting client to server
-print(f'Welcome to the server {user}!')
-receive_thread = threading.Thread(target=receiveMessage)               #receiving multiple messages
-receive_thread.start()
-write_thread = threading.Thread(target=writeMessage)                   #sending messages 
-write_thread.start()
+ip, user, port = getParameters()
+try:
+    #try and connect to the server
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      #socket initialization
+    clientSocket.connect((ip, port))
+    receive_thread = threading.Thread(target=receiveMessage)               #receiving multiple messages
+    receive_thread.start()
+    write_thread = threading.Thread(target=writeMessage)                   #sending messages 
+    write_thread.start()
+except:
+    #if something is wrong.
+    print('Server may be unavailable or the IP address / port number is incorrect. \nCheck IP address and host and try again later')
+    sys.exit()
+
+# cd desktop/du/year 2/networks and systems/cw/networks
+# client.py 192.168.139.1 liv 8080
--- CW/Networks/server.py
+++ CW/Networks/server.py
@@ -3,6 +3,7 @@ import sys
 import threading
 
 def getParameters():
+    #gets port number from command line
     try: 
         port = sys.argv[1]
         port = int(port)
@@ -11,49 +12,82 @@ def getParameters():
         print("Port number must be an integer")
         sys.exit()                                      
 
-def broadcast(message):                                                 #broadcast function declaration
+def broadcast(message, currentClient):
+    #sends a message to each client
     for client in clients:
         client.send(message)
 
-def handle(client):                                         
-    while True:
-        try:                                                            #recieving valid messages from client
+def handle(client):
+    #handles the clients
+    while 1:
+        try:
+            #receive a valid message from a client
             message = client.recv(1024)
-            broadcast(message)
+            broadcast(message, client)
             print(message.decode())
-        except:                               
+            logFile.write(message.decode())
+            print(logFile.closed)
+        except:
+            #if the message is invalid, or something goes wrong.
             clients.remove(client)
             client.close()
-            nickname = nicknames[client]
-            del nicknames[client]
-            broadcast('{} left!'.format(nickname).encode())
+            nickname = usernames[client]
+            del usernames[client]
+            broadcast('{} left!'.format(nickname).encode(),'')
             print(f'{nickname} left!')
+            logFile.write(f'{nickname} left\n')
             break
 
-def receiveConnection():                                                          #accepting multiple clients
-    while True:
-        client, address = serverSocket.accept() 
-        client.send('NICKNAME'.encode())
-        nickname = client.recv(1024).decode()
-        nicknames[client] = nickname
-        clients.append(client)
-        print(f'{nickname} joined at {str(address)}')
-        broadcast("{} joined!".format(nickname).encode())
-        client.send('Connected to server!'.encode())
-        receiveThread = threading.Thread(target=handle, args=(client,))
-        receiveThread.start()
+def write():
+    while 1:
+        try:
+            message = input('')
+            if message == 'q':
+                print(message)
+        except KeyboardInterrupt:
+            print('keyboard interrupt')
+        #     logFile.close()
+        #     serverSocket.close()
+        #     print(logFile.closed)
+        #     print('server closed')
+        #     sys.exit() #won't work
+
+def receiveConnection():
+    #accepts multiple clients
+    while 1:
+        try:
+            client, address = serverSocket.accept() 
+            # receives nickname from client
+            client.send('USERNAME'.encode())
+            nickname = client.recv(1024).decode()
+            usernames[client] = nickname
+            clients.append(client)
+            print(f'{nickname} joined at {str(address)}')
+            logFile.write(f'{nickname} joined at {str(address)}\n')
+            print(logFile.closed)
+            broadcast("{} joined!".format(nickname).encode(),client)
+            client.send(f'Welcome to the server {nickname}!'.encode())
+            receiveThread = threading.Thread(target=handle, args=(client,))
+            receiveThread.start()
+            writeThread = threading.Thread(target=write)
+            writeThread.start()
+        except KeyboardInterrupt:
+            print('interrupt')
+            break
 
 
-ip = '192.168.139.1'                                                      #LocalHost
-port = getParameters()                                                             #Choosing unreserved port
-
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)              #socket initialization
-serverSocket.bind((ip, port))                                               #binding host and port to socket
+ip = '192.168.139.1'
+port = getParameters()
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    
+serverSocket.bind((ip, port))
 serverSocket.listen()
 print(f'Listening at {ip}:{port}')
-
-clients = []
-nicknames = {}
-receiveConnection()
-
+with open('server.log', 'w+') as logFile:
+# logFile = open('server.log','w')
+    logFile.write(f'Listening at {ip}:{port}\n')
+    # logFile.close()
+    clients = []
+    usernames = {}
+    receiveConnection()
 # cd desktop/du/year 2/networks and systems/cw/networks
+# server.py 8080
